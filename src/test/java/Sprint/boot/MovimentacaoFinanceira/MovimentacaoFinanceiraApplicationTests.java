0ad795a6675d92a754aa6085c88b21edb39cf05a
bispobr <156684382+bispobr@users.noreply.github.com>
2026-09-05 11:29:32 -0300

    feat: add classe  main
    habilita profile de testes

---
+++ src/test/java/Sprint/boot/MovimentacaoFinanceira/MovimentacaoFinanceiraApplicationTests.java
@@ -0,0 +1,15 @@
+package Sprint.boot.MovimentacaoFinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MovimentacaoFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
