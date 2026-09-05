1bfa9cfc5e31798f113c4453a29b68414ae8be7d
bispobr <156684382+bispobr@users.noreply.github.com>
2026-09-05 10:39:58 -0300

    feat: add classe  main

---
+++ src/main/java/Sprint/boot/MovimentacaoFinanceira/MovimentacaoFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package Sprint.boot.MovimentacaoFinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimentacaoFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimentacaoFinanceiraApplication.class, args);
+	}
+
+}
